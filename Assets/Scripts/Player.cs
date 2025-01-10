58e1bae17a93d5c44ea9d23a134ded96c97c036b
Zoffy0717 <149544226+Zoffy0717@users.noreply.github.com>
2025-01-10 12:08:28 -0800

    Done

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,11 +18,35 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left * _speed*Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right * _speed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down * _speed * Time.deltaTime);
+        }
+        PlantSeed();
+        _plantCountUI.UpdateSeeds(_numSeedsPlanted, _numSeeds);
     }
 
     public void PlantSeed ()
     {
+            if(Input.GetKeyDown(KeyCode.Space) && _numSeeds > 0)
+            {
+                Instantiate(_plantPrefab,_playerTransform.position, _playerTransform.rotation);
+                
+                _numSeeds--;
+                _numSeedsPlanted++;
+            }
         
     }
 }
